d64bd31de5e0aa1478a3d1375ed9f02d03f68769
McoderPY <95954539+McoderPY@users.noreply.github.com>
2021-12-11 12:01:18 +0530

    1stproject.py

---
+++ 1st project.py
@@ -0,0 +1,7 @@
+password=input("enter your password")
+username = input("enter your username")
+age = int(input("Enter your age"))
+
+if password == "A"and username == "B" and age == 14 : print("welcome A")
+
+else:print("login failed")password
